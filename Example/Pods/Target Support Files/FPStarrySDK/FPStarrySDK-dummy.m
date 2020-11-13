92590830ebb7cbfe526bc28de05a40588c87f6c6
fan <447690182@qq.com>
2020-11-13 10:18:21 +0800

    firt commit

---
+++ Example/Pods/Target Support Files/FPStarrySDK/FPStarrySDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_FPStarrySDK : NSObject
+@end
+@implementation PodsDummy_FPStarrySDK
+@end
---
+++ FPStarrySDK/Classes/FPStarrySDK.swift
@@ -0,0 +1,15 @@
+//
+//  FPStarrySDK.swift
+//  FPStarryNetwork
+//
+//  Created by StarryMedia on 2020/11/13.
+//
+
+import UIKit
+
+public class FPStarrySDK: NSObject {
+
+    open func testSDK() {
+        print("testtest")
+    }
+}
